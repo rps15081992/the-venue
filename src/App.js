bef66b215c73b4c31849421888797c0896d446c2
Ramprit Sahani <rampritsahani@outlook.in>
2018-11-18 14:08:45 +0530

    highlights added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
-import Featured from "./components/Featured/Index";
+import Featured from "./components/Featured";
+import VunueNfo from "./components/VenueNFO";
+import HighLight from "./components/HighLights";
+
 class App extends Component {
   render() {
     return (
-      <div style={{ height: "1500px", background: "cornflowerblue" }}>
+      <div
+        className="App"
+        style={{ height: "1500px", background: "white" }}
+      >
         <Header />
         <Featured />
+        <VunueNfo />
+        <HighLight />
       </div>
     );
   }
